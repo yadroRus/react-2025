412637c6c8c2a6d124066e8342b1b3c8d1cac891
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-07 20:18:06 +0300

    Создание начальной странички с выводом списка ресторанов (#4)
    * настройка vite
    
    * добавлен претиер
    
    * второе дз - список ресторанов
    
    * чистка ненужного
    
    * мелкий рафакторинг
    
    * добавленны доп функции
    
    * переразбивка по компонентов

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {restaurants} from '../materials/mock.js'
+import RestaurantTile from "./restaurant-tile/restaurant-tile.jsx";
+
+function App() {
+    return (
+        <>
+            <h1>Рестораны</h1>
+            {restaurants.map((restaurant) => <RestaurantTile key={restaurant.id} {...restaurant}/>)}
+        </>
+    )
+}
+
+export default App
---
+++ src/restaurant-tile/restaurant-tile.jsx
@@ -0,0 +1,15 @@
+import './restaurant-tile.css';
+import RestaurantComments from "../restaurant-comments/restaurant-comments.jsx";
+import RestaurantMenu from "../restaurant-menu/restaurant-menu.jsx";
+
+function RestaurantTile({name, menu, reviews}) {
+    return (
+        <div className='restaurant-tile'>
+            <h2 className='restaurant-tile__name'>{name}</h2>
+            {RestaurantMenu(menu)}
+            {RestaurantComments(reviews)}
+        </div>
+    )
+}
+
+export default RestaurantTile;
