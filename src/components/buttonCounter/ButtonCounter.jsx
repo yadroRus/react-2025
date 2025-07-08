18d8ee28c33ccf79c5e6030b021245fd7d11b92c
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-08 15:38:03 +0300

    добавлен useImperativeHandle в счетчик

--- src/components/buttonCounter/ButtonCounter.jsx
+++ src/components/buttonCounter/ButtonCounter.jsx
@@ -1,12 +1,19 @@
 import {useCounter} from "./helpers.js";
 import './button-counter.css'
+import {useImperativeHandle} from "react";
 
-function ButtonCounter({max, min}) {
+function ButtonCounter({max, min, ref}) {
     const [count, increment, decrement] = useCounter(0);
+
+    // works like an outer api of a component
+    useImperativeHandle(ref, () => ({
+        getCount: () => count
+    }), [count]);
+
     return (
         <div className='btn-counter'>
             <button className='btn-counter__button' onClick={() => decrement(min)}>-</button>
-            {count}
+            <span ref={ref}>{count}</span>
             <button className='btn-counter__button' onClick={() => increment(max)}>+</button>
         </div>
     )
--- src/components/restaurant-menu-item/restaurant-menu-item.jsx
+++ src/components/restaurant-menu-item/restaurant-menu-item.jsx
@@ -1,19 +1,31 @@
 import './restaurant-menu-item.css';
 import ButtonCounter from "../buttonCounter/ButtonCounter.jsx";
+import React, {useRef} from "react";
 
 function RestaurantMenuItem({name, price, ingredients}) {
+    const [totalPrice, setTotalPrice] = React.useState(0);
+    const counterRef = useRef(null);
+
+    const updatePrice = () => {
+        setTimeout(() => {
+            // todo: Андрей, какой вариат предпочтительней через useImperativeHandle или чтение знаяения из DOM?
+            setTotalPrice(counterRef.current.getCount() * price);
+            // setTotalPrice(counterRef.current.textContent * price || price);
+        })
+    }
+
     return (<li className='restaurant-menu-item'>
-            <div className='restaurant-menu-item__text'>
-                <span>{name}</span>
-                <div className='restaurant-menu-item__counter'>
-                    <span>{price} руб.</span>
-                    <ButtonCounter min="0" max="5"/>
-                </div>
+        <div className='restaurant-menu-item__text'>
+            <span>{name} <span style={{fontSize: '0.8em'}}>({price} руб.)</span></span>
+            <div className='restaurant-menu-item__counter' onClick={updatePrice}>
+                <span>{totalPrice} руб.</span>
+                <ButtonCounter min="0" max="5" ref={counterRef}/>
             </div>
-            <div className='restaurant-menu-item__labels'>
-                {ingredients.map((item, i) => <span key={i} className='restaurant-menu-item__label'>{item}</span>)}
-            </div>
-        </li>)
+        </div>
+        <div className='restaurant-menu-item__labels'>
+            {ingredients.map((item, i) => <span key={i} className='restaurant-menu-item__label'>{item}</span>)}
+        </div>
+    </li>)
 }
 
 export default RestaurantMenuItem;
