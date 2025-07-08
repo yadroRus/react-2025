49f92cd827fc784348cbdad52bc5b10d9cac0df1
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-08 15:51:53 +0300

    добавлены состояния кнопок

--- src/components/buttonCounter/ButtonCounter.jsx
+++ src/components/buttonCounter/ButtonCounter.jsx
@@ -3,18 +3,21 @@ import './button-counter.css'
 import {useImperativeHandle} from "react";
 
 function ButtonCounter({max, min, ref}) {
-    const [count, increment, decrement] = useCounter(0);
+    const [count, increment, decrement, incrementDisabled, decrementDisabled] = useCounter(0);
 
     // works like an outer api of a component
     useImperativeHandle(ref, () => ({
         getCount: () => count
     }), [count]);
 
+
     return (
         <div className='btn-counter'>
-            <button className='btn-counter__button' onClick={() => decrement(min)}>-</button>
+            <button className='btn-counter__button' disabled={decrementDisabled} onClick={() => decrement(min)}>-
+            </button>
             <span ref={ref}>{count}</span>
-            <button className='btn-counter__button' onClick={() => increment(max)}>+</button>
+            <button className='btn-counter__button' disabled={incrementDisabled} onClick={() => increment(max)}>+
+            </button>
         </div>
     )
 }
