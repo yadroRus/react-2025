29f44b7b6248a1fe35242b28eff09ffc0e45fc6a
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-11 23:39:25 +0300

    добавлен scroll bar progress

---
+++ src/components/scroll-progress/scroll-progress.jsx
@@ -0,0 +1,32 @@
+import "./scroll-progress.css";
+import { useEffect, useRef } from "react";
+
+function getVerticalScrollPercentage(node) {
+  const parent = node.parentNode;
+  return (node.scrollTop || parent.scrollTop) / (parent.scrollHeight - parent.clientHeight) * 100;
+}
+
+export const ScrollProgress = () => {
+
+  const refProgressBar = useRef(null);
+
+  useEffect(() => {
+
+    const onScroll = () => {
+      refProgressBar.current.style.transform = `translateX(${getVerticalScrollPercentage(document.body) - 100}%)`;
+    };
+
+    window.addEventListener("scroll", onScroll);
+
+    return () => {
+      window.removeEventListener("scroll", onScroll);
+    };
+
+  }, []);
+
+  return (
+    <div className="scroll-progress">
+      <div ref={refProgressBar} className="scroll-progress-bar"></div>
+    </div>
+  );
+};
