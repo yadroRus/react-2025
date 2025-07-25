03349bfa517fd5fa8263300e531dd86bece5ffc0
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-25 15:58:13 +0300

    правка замечаний

--- src/components/cart/hooks.js
+++ src/components/cart/hooks.js
@@ -1,23 +1,24 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useCart = () => {
   const [opened, setOpen] = useState(false);
-  const openedState = useRef(null);
 
   const toggleCart = (value) => {
-    openedState.current = value;
     setOpen(value);
   };
 
   useEffect(() => {
     const onBodyClick = (e) => {
-      if (
-        openedState.current &&
-        e.target.isConnected &&
-        !e.target.closest(".js-cart-area")
-      ) {
-        toggleCart(false);
-      }
+      setOpen((current) => {
+        if (
+          current &&
+          e.target.isConnected &&
+          !e.target.closest(".js-cart-area")
+        ) {
+          return false;
+        }
+        return current;
+      });
     };
     document.body.addEventListener("click", onBodyClick);
     return () => {
--- src/components/dish-counter/hooks.js
+++ src/components/dish-counter/hooks.js
@@ -1,32 +1,29 @@
 import {
   addToCart,
   removeFromCart,
-  selectCartDishById,
+  selectCartDishById
 } from "../../data/entities/cart/slice.js";
 import { useDispatch, useSelector } from "react-redux";
-import { useCallback, useRef } from "react";
+import { useCallback } from "react";
 
 export const useCounter = ({ min, max, dishId, price = 0 }) => {
   const dispatch = useDispatch();
   const cartDish = useSelector((state) =>
-    selectCartDishById(state, dishId),
+    selectCartDishById(state, dishId)
   ) || { count: 0, totalPrice: 0 };
-  const countRef = useRef(null);
   const { count, totalPrice } = cartDish;
 
-  countRef.current = count;
-
   const increment = useCallback(() => {
-    if (max === undefined || countRef.current < Number(max)) {
+    if (max === undefined || count < Number(max)) {
       dispatch(addToCart({ id: dishId, price }));
     }
-  }, [max, dishId, price, dispatch]);
+  }, [max, dishId, price, dispatch, count]);
 
   const decrement = useCallback(() => {
-    if (min === undefined || countRef.current > Number(min)) {
+    if (min === undefined || count > Number(min)) {
       dispatch(removeFromCart({ id: dishId, price }));
     }
-  }, [min, dishId, price, dispatch]);
+  }, [min, dishId, price, dispatch, count]);
 
   return { count, totalPrice, increment, decrement };
 };
