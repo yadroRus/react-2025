55b7aed7ed6ba7ce52766aa1f4aca524d03572eb
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-28 19:10:52 +0300

    удалил лешние файлы

--- src/pages/restaurant-page-container.jsx
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-import { selectRestaurantsIds } from "../data/entities/restuarants/slice.js";
-import { RestaurantPage } from "./restaurant-page.jsx";
-
-export const RestaurantPageContainer = () => {
-  const restaurantsIds = useSelector(selectRestaurantsIds);
-  return <RestaurantPage restaurantsIds={restaurantsIds}></RestaurantPage>;
-};
