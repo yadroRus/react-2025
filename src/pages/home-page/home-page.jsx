9e93ff3f79847ed8fb1a0677608ce8d3d189b906
yadroRus <37119397+yadroRus@users.noreply.github.com>
2025-07-29 03:22:29 +0300

    больше текста на главную

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -1,11 +1,21 @@
 import { Link } from "../../components/link/link.jsx";
 import { RESTAURANT_PAGE } from "../links-paths.js";
+import { Breadcrumbs } from "../../components/breadcrumbs/breadcrumbs.jsx";
 
 export const HomePage = () => {
   return (
     <>
-      <h1> Главная страница</h1>
-      <Link to={RESTAURANT_PAGE}>Рестораны</Link>
+      <Breadcrumbs />
+      <h1>Добро пожаловать!</h1>
+      <br />
+      <p>Самые свежие булочки только у нас!</p>
+      <br />
+      <p>Тут можно не только поесть, но и попить: <Link to={RESTAURANT_PAGE}>рестораны</Link></p>
+      <br />
+      <h2>Новости</h2>
+      <ul>
+        <li>Ресторан "Смятка" проводит акцию последнего пельменя!</li>
+      </ul>
     </>
   );
 };
